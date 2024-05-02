60833336b8def518faea737304db753d42d9b8ad
shimoch <mikadonoosoba2@gmail.com>
2024-05-02 15:04:21 +0900

    cdk impol

--- apps/cdk-sample/lib/cdk-sample-stack.ts
+++ apps/cdk-sample/lib/cdk-sample-stack.ts
@@ -1,16 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as amplify from "aws-cdk-lib/aws-amplify";
+import type { Construct } from "constructs";
 
 export class CdkSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const app = new amplify.CfnApp(this, "AmplifyApp", {
+      name: "cdk-import-sample-apps/sample-app",
+      accessToken: "dummy",
+      environmentVariables: [
+        {
+          name: "AMPLIFY_DIFF_DEPLOY",
+          value: "false",
+        },
+        {
+          name: "AMPLIFY_MONOREPO_APP_ROOT",
+          value: "apps/sample-app",
+        },
+      ],
+    });
+    new amplify.CfnBranch(this, "Branch", {
+      appId: app.attrAppId,
+      branchName: "main",
+    });
   }
 }
